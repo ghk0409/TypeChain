bde6cf8f7c944b307866ac7e3a2aba5aadf107b4
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2023-01-06 21:24:01 +0900

    블록체인 이론 구현 완성

--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,40 @@ class Block implements BlockShape {
         return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class Blockchain {
+    private blocks: Block[];
+    constructor() {
+        this.blocks = [];
+    }
+
+    private getPrevHash() {
+        if (this.blocks.length === 0) return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+
+    public addBlock(data: string) {
+        const newBlock = new Block(
+            this.getPrevHash(),
+            this.blocks.length + 1,
+            data
+        );
+        this.blocks.push(newBlock);
+    }
+
+    public getBlocks() {
+        // 신규 배열을 리턴, 원본 blockchain 배열을 건드릴 수 없게끔 보안!
+        return [...this.blocks];
+    }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("first");
+blockchain.addBlock("second");
+blockchain.addBlock("third");
+
+// 원본 배열이 보호되었기 때문에 변화 없음
+blockchain.getBlocks().push(new Block("xxxxx", 1111111, "HACKEDDDDDD zzzz"));
+
+console.log(blockchain.getBlocks());
